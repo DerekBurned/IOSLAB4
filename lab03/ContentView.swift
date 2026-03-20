48c2693507470969c7bd3fc0b3597bb631afe5cd
Derek <danillolka2@gmail.com>
2026-03-20 16:17:26 +0100

    Initial Commit

---
+++ lab03/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lab03
+//
+//  Created by Danylo Lukianiuk on 20/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lab03/lab03App.swift
@@ -0,0 +1,17 @@
+//
+//  lab03App.swift
+//  lab03
+//
+//  Created by Danylo Lukianiuk on 20/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct lab03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
